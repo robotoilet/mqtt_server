37b6fe5c72caf44b67b03b1d540bc8af0963caa1
Alecs Geuder <al.geuder@gmail.com>
2014-11-19 13:28:22 +0000

    adapted to global config module

--- test/test_server.js
+++ test/test_server.js
@@ -9,14 +9,16 @@ describe('incoming_mqtt', function() {
 
   it('should accept an mqtt publish by a known client and call a given distribute function with expected arguments', function(done) {
 
-    msg =  'sensorX (1234, 567) (123 456)';
+    var msg =  'SensorX (1234, 567) (123 456)';
+    var punter = 'punterX';
+    var site = 'siteX';
 
     // overwrite the default distribute function as a mock
     server.distribute = function(auth, dataString, callback) {
       auth.should.eql(
-        { id: 'clientX_toiletX',
-          username: 'clientX',
-          password: 'clientX'
+        { id: punter + '_' + site,
+          username: punter,
+          password: punter
         }
       );
       dataString.should.equal(msg);
@@ -27,9 +29,9 @@ describe('incoming_mqtt', function() {
     server.listen(1884)
 
     client = mqtt.createClient(1884, 'localhost', {
-      clientId: "toiletX",
-      username: "clientX",
-      password: "clientX"
+      clientId: site,
+      username: punter,
+      password: punter
     });
     client.publish('unimportant_tag', msg);
     client.end();
